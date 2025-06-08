76d8220a6ff2421d6f521a0536d2b26f4eae7b7e
Dev-anshika98 <g.anshikacreative@gmail.com>
2025-06-08 17:44:00 +0530

    route update

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectdb= async()=>{
     try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/test`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log(`\n mongodb connected \n ${connectionInstance.connection.host}`);
     }catch(error){
         console.log(error);
